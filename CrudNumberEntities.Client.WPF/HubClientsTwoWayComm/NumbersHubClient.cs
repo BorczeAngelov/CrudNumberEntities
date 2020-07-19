ee11c8768a339d8a78c101f2771fb6051b40757e
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-19 23:04:59 +0200

    CrudNumberEntities.Client.WPF: - Add NumbersHubClient

---
+++ CrudNumberEntities.Client.WPF/HubClientsTwoWayComm/NumbersHubClient.cs
@@ -0,0 +1,78 @@
+﻿using CrudNumberEntities.Common.DataModels;
+using CrudNumberEntities.Common.HubInterfaces;
+using CrudNumberEntities.Common.Utils;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace CrudNumberEntities.Client.WPF.HubClientsTwoWayComm
+{
+    internal class NumbersHubClient : INumbersHubClientTwoWayComm
+    {
+        private readonly HubConnection _connection;
+
+        public event Action<NumberEntitiy> NumberCreated;
+        public event Action<NumberEntitiy> NumberUpdated;
+        public event Action<NumberEntitiy> NumberDeleted;
+
+        public INumbersHub ServerHub { get; }
+
+        internal NumbersHubClient()
+        {
+            _connection = new HubConnectionBuilder()
+               .WithUrl(DemoUrlConstants.LocalHostUrl + DemoUrlConstants.NumbersHubEndpoint)
+               .Build();
+
+            _connection.On<NumberEntitiy>(nameof(InvokeCreate), InvokeCreate);
+            _connection.On<NumberEntitiy>(nameof(InvokeUpdate), InvokeUpdate);
+            _connection.On<NumberEntitiy>(nameof(InvokeDelete), InvokeDelete);
+
+            ServerHub = new NumbersHubImp(_connection);
+        }
+
+        public async Task ConnectWithServerHub()
+        {
+            await _connection.StartAsync();
+        }
+
+        public void InvokeCreate(NumberEntitiy number)
+        {
+            NumberCreated?.Invoke(number);
+        }
+
+        public void InvokeUpdate(NumberEntitiy number)
+        {
+            NumberUpdated?.Invoke(number);
+        }
+
+        public void InvokeDelete(NumberEntitiy number)
+        {
+            NumberDeleted?.Invoke(number);
+        }
+
+        private class NumbersHubImp : INumbersHub
+        {
+            private readonly HubConnection _connection;
+
+            public NumbersHubImp(HubConnection connection)
+            {
+                this._connection = connection;
+            }
+
+            public Task Create()
+            {
+                return _connection.InvokeAsync(nameof(Create));
+            }
+
+            public Task Update(NumberEntitiy number)
+            {
+                return _connection.InvokeAsync(nameof(Update), number);
+            }
+
+            public Task Delete(NumberEntitiy number)
+            {
+                return _connection.InvokeAsync(nameof(Delete), number);
+            }
+        }
+    }
+}
--- CrudNumberEntities.Client.WPF/MainWindow.xaml.cs
+++ CrudNumberEntities.Client.WPF/MainWindow.xaml.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CrudNumberEntities.Client.WPF.HubClientsTwoWayComm;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CrudNumberEntities.Client.WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly NumbersHubClient _numbersHubClient;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _numbersHubClient = new NumbersHubClient();
+        }
+
+        private async void OnConnectClick(object sender, RoutedEventArgs e)
+        {
+            await _numbersHubClient.ConnectWithServerHub();
         }
     }
 }
