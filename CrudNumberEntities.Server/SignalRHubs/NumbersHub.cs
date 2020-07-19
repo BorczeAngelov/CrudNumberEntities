1a9bb67c5ba1d9f3e9c4790d9ffbe311e77f1de2
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-19 23:01:56 +0200

    CrudNumberEntities.Server: - Create (not complete implemented) NumberHub class - Map NumberHub in startup

---
+++ CrudNumberEntities.Server/SignalRHubs/NumbersHub.cs
@@ -0,0 +1,37 @@
+﻿using CrudNumberEntities.Common.DataModels;
+using CrudNumberEntities.Common.HubInterfaces;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace CrudNumberEntities.Server.SignalRHubs
+{
+    internal class NumbersHub : Hub, INumbersHub
+    {
+        public override Task OnConnectedAsync()
+        {
+            //TODO: read current global state
+            return base.OnConnectedAsync();
+        }
+
+        public async Task Create()
+        {
+            throw new NotImplementedException();
+            NumberEntitiy number = null;
+            await Clients.All.SendAsync(nameof(INumbersHubClient.InvokeCreate), number);
+        }
+
+        public async Task Update(NumberEntitiy number)
+        {
+            throw new NotImplementedException();
+            NumberEntitiy updatedNumber = null;
+            await Clients.All.SendAsync(nameof(INumbersHubClient.InvokeUpdate), updatedNumber);
+        }
+
+        public async Task Delete(NumberEntitiy number)
+        {
+            throw new NotImplementedException();
+            await Clients.All.SendAsync(nameof(INumbersHubClient.InvokeDelete), number);
+        }
+    }
+}
--- CrudNumberEntities.Server/Startup.cs
+++ CrudNumberEntities.Server/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CrudNumberEntities.Common.Utils;
+using CrudNumberEntities.Server.SignalRHubs;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,7 +30,7 @@ namespace CrudNumberEntities.Server
 
             app.UseEndpoints(endpoints =>
             {
-                //endpoints.MapHub<TextBoxHub>(DemoConstants.TextBoxHubEndpoint);
+                endpoints.MapHub<NumbersHub>(DemoUrlConstants.NumbersHubEndpoint);
             });
         }
     }
